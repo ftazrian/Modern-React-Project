cfddbf02fec52f4fddaaed2117a166f3fe431ff2
ftazrian <ftazrian110201@gmail.com>
2025-08-12 13:01:10 +0600

    first commit

---
+++ src/components/MonitorSection.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+import monitorcard from "../assets/monitorcard.webp";
+import { FaArrowRightLong } from "react-icons/fa6";
+
+const MonitorSection = () => {
+  return (
+    <section className="max-w-7xl mx-auto px-4 py-16 md:py-24">
+      <div className="flex flex-col md:flex-row items-center justify-between gap-12 md:gap-24">
+        <div className="md:w-1/2 w-full">
+          <p className="text-green-500 font-semibold">MONITOR</p>
+          <h2 className="text-3xl md:text-4xl font-bold text-navy-900 mt-4 mb-6 md:w-4/5">
+            Introducing best mobile carousels
+          </h2>
+
+          <p className="text-gray-600 mb-8">
+            Take control of your time and boost productivity with our
+            intelligent scheduling system. Automate appointments, manage team
+            availability, and deliver exceptional customer experiences through
+            seamless calendar management.
+          </p>
+           <a
+          href="#"
+          className="text-blue-500 font-semibold flex items-center gap-2 hover:gap-4 transition-all"
+        >
+          Explore scheduling features  <FaArrowRightLong className="size-5" />
+        </a>
+       
+        </div>
+
+        <div className="md:w-1/2 w-full">
+          <img src={monitorcard} alt="stats image" className="w-full h-auto" />
+        </div>
+       
+      </div>
+    </section>
+  );
+};
+
+export default MonitorSection;
